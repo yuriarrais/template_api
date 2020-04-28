5a140c6ef16ccbfe3250835911a2bbeab121a359
Arthur <sarthursantos@gmail.com>
2020-04-28 09:43:59 -0300

    Adicionando query search_all em query.py

--- src/app/infrastructure/query.py
+++ src/app/infrastructure/query.py
@@ -1,9 +1,17 @@
+
+
 def query_insert(table, attributes, parameters):
     prefix = f'INSERT INTO {table} '
     var_attributes = _make_attributes(attributes)
     var_param = _make_param(attributes)
     sql_statement = prefix + var_attributes + var_param
 
+    return sql_statement
+
+
+def query_search_all(table, attributes):
+    var_attributes = _make_attributes(attributes)[1:-8]
+    sql_statement = f'SELECT {var_attributes} FROM {table} '
     return sql_statement
 
 
